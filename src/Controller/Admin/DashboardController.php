7510bb6c60112b7d77f4de39d88bbd27aae65028
ClemBlue <clement.blin76@gmail.com>
2021-11-17 12:20:49 +0100

    Intergace admin pret a etre modifier et faire des tests

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -8,6 +8,8 @@ use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Conteneur;
+use App\Entity\Users;
 
 class DashboardController extends AbstractDashboardController
 {
@@ -16,7 +18,11 @@ class DashboardController extends AbstractDashboardController
      */
     public function index(): Response
     {
-        return parent::index();
+        //return parent::index();
+        $routeBuilder = $this->get(AdminUrlGenerator::class);
+        $url = $routeBuilder->setController(ConteneurCrudController::class)->generateUrl();
+        
+        return $this->redirect($url);
     }
 
     public function configureDashboard(): Dashboard
@@ -27,7 +33,10 @@ class DashboardController extends AbstractDashboardController
 
     public function configureMenuItems(): iterable
     {
-        yield MenuItem::linktoDashboard('Dashboard', 'fa fa-home');
+        //yield MenuItem::linktoDashboard('Dashboard', 'fa fa-home');
         // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+        yield MenuItem::linktoRoute('Back to the website', 'fas fa-home', 'homepage');
+        yield MenuItem::linkToCrud('Conteneur', 'fas fa-map-marker-alt', Conteneur::class);
+        yield MenuItem::linkToCrud('Users', 'fas fa-map-marker-alt', Users::class);
     }
 }
