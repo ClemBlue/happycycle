de536f667fc5c9693201945653f470ffbec6013a
ClemBlue <clement.blin76@gmail.com>
2021-12-09 14:09:07 +0100

    Page Login + Register terminé

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -16,14 +18,16 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
+            ->add('email', EmailType::class, [
+                'attr' => ['placeholder' => 'Adresse mail'],
+            ])
             ->add('plainPassword', RepeatedType::class, [
                 'type' => PasswordType::class,
                 'invalid_message' => 'Les champs doivent être identiquent.',
                 'options' => ['attr' =>['class' => 'password-field']],
                 'required' => true,
-                'first_options' => ['label' => 'Mot de passe'],
-                'second_options' => ['label' => 'Confirmer Mot de passe'],
+                'first_options' => ['attr' => ['placeholder' => 'Mot de passe']],
+                'second_options' => ['attr' =>  ['placeholder' => 'Confirmer Mot de passe']],
                 'mapped' => false,
                 'attr' => ['autocomplete' => 'new-password'],
                 'constraints' => [
@@ -38,10 +42,18 @@ class RegistrationFormType extends AbstractType
                     ]),
                 ],
             ])
-            ->add('nom')
-            ->add('prenom')
-            ->add('adresse')
-            ->add('codePostal')
+            ->add('nom', TextType::class, [
+                'attr' => ['placeholder' => 'Nom'],
+            ])
+            ->add('prenom', TextType::class, [
+                'attr' => ['placeholder' => 'Prénom'],
+            ])
+            ->add('adresse', TextType::class, [
+                'attr' => ['placeholder' => 'Adresse'],
+            ])
+            ->add('codePostal', TextType::class, [
+                'attr' => ['placeholder' => 'Code Postal'],
+            ])
         ;
     }
 
